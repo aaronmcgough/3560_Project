fab96b2bdc071f1ea5cd73a31050165db63aa313
aaronmcgough <64175777+aaronmcgough@users.noreply.github.com>
2022-04-27 22:33:46 -0500

    Add files via upload

---
+++ 001-goal.1m.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Metadata allows your plugin to show up in the app, and website.
+#
+#  <xbar.title>Goals</xbar.title>
+#  <xbar.version>v1.0</xbar.version>
+#  <xbar.author>Aaron McGough</xbar.author>
+#  <xbar.author.github>arm2mc</xbar.author.github>
+#  <xbar.desc>This plugin is a script that is part of my final project in my 3560 class.</xbar.desc>
+#
+#  <xbar.dependencies>Bash</xbar.dependencies>
+#  <xbar.abouturl>http://url-to-about.com/</xbar.abouturl>
+
+#<xbar.var>string(VAR_GOAL="goal"): Your goal.</xbar.var>
+
+
+echo $VAR_GOAL
